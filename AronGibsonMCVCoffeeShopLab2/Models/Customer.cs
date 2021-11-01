e9db64b397545c6bb86fdf7a431d132ea89a16f0
aronunlabeled <xaron97x@hotmail.com>
2021-11-01 19:12:15 -0400

    first

---
+++ AronGibsonMCVCoffeeShopLab2/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace AronGibsonMCVCoffeeShopLab2
+{
+    public partial class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool LactoseIntolerant { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
